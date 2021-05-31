84bb86997bd42b534d4a789ecaf406841afe5f97
Shpagat <39337275+Shpagat@users.noreply.github.com>
2021-05-31 10:40:50 +0300

    Add files via upload

---
+++ get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import pika
+import time
+
+credentials = pika.PlainCredentials('ruser', 'ruser')
+params = pika.ConnectionParameters('64.227.121.66',
+                                       5672,
+                                       '/',
+                                       credentials)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body.decode())
+    time.sleep(body.count(b'.'))
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='MARK SIGNALS', on_message_callback=callback)
+
+channel.start_consuming()
