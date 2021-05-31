a6f5224508ea81f1f04f81d7ba7b59c72b068ebe
Shpagat <39337275+Shpagat@users.noreply.github.com>
2021-05-31 10:58:32 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,2 +1,18 @@
-def print():
-    print ('hello world')
+import RPi.GPIO as GPIO
+import time
+GPIO_PWM_0 = 12
+FREQUENCY = 100
+DELAY_TIME = 0.02
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PWM_0, GPIO.OUT)
+pwmOutput_0 = GPIO.PWM(GPIO_PWM_0, FREQUENCY)
+pwmOutput_0.start(0)
+try:
+    while True:
+        for dutyCycle in range(0, 101, 1):
+            pwmOutput_0.ChangeDutyCycle(dutyCycle) 
+            sleep(DELAY_TIME)
+except KeyboardInterrupt:
+    pwmOutput_0.stop()
+    GPIO.cleanup()
+    print('exiting')
