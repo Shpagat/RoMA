3c2daa8e00c6deed238fb5c46837256758bea953
Shpagat <asif.gusejnov.1997@mail.ru>
2021-06-08 12:50:54 +0300

    nothing

--- functions.py
+++ functions.py
@@ -93,16 +93,7 @@ def qr_detector():
 
     # нахождение и декодирование нашего кода
     while True:
-<<<<<<< HEAD
-        for dutyCycle in range(0, 101, 1):
-            pwmOutput_0.ChangeDutyCycle(dutyCycle)
-            time.sleep(DELAY_TIME)
-    except KeyboardInterrupt:
-    pwmOutput_0.stop()
-    GPIO.cleanup()
-    print('exiting')
-=======
-        # получить изображение
+      # получить изображение
         _, img = cap.read()
 
         # извлечь местоположение штрих-кода и зашифрованную информацию
@@ -116,8 +107,6 @@ def qr_detector():
             cv2.destroyAllWindows()
 
             return key
-
-
 # печатаем значение в консоли
 print(qr_detector())
->>>>>>> d871baf733f8898117a82c85ec6db405bb4bfefb
+
