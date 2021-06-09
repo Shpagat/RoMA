a55c1de30e0e5f84ce37fa1c20309c83c395f5ae
Shpagat <asif.gusejnov.1997@mail.ru>
2021-06-09 19:09:26 +0300

    fixed some bugs

--- main.py
+++ main.py
@@ -10,18 +10,27 @@ counter = 1
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(motor, GPIO.OUT)
 
+# создание словаря
+adress = {
+    '100000001': 100000001,
+    '100000010': 100000010,
+    '100000100': 100000100,
+    '100001000': 100001000,
+    '100010000': 100010000
+}
 
 while KeyboardInterrupt():
     # запуск сканирования qr-кода
-    adress = (str(functions.qr_detector))
-
-    while counter <= 5:
-
-        if functions.ir_sensor(ir_pin) == 1:  # ожидание датчика
-            functions.shift(adress)  # открытие ячейки
-            counter += 1
-        else:
-            print('Not found id')
+    key = (functions.qr_detector)
+    if key in adress:
+        print (adress[key])
+        while counter <= 5:
+            if functions.ir_sensor(ir_pin) == 1:  # ожидание датчика
+                functions.shift(adress)  # открытие ячейки
+                print (key)
+                counter += 1
+            else:
+                print('Not found id')
 
     while functions.sphi_sensor(sphi_pin) == 0:   # закрытие ячеек
         GPIO.output(motor, 1)
